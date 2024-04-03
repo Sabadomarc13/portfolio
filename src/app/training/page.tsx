01453a14058379b545d274dafb9fba8bbe8026d8
Sabadomarc13 <sabado@pinyastudio.com>
2024-04-03 23:13:57 +0800

    asdasd

--- src/app/training/page.tsx
+++ src/app/training/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="px-10 py-9 w-full gap-5 flex flex-col">
       <h1 className="font-semibold text-3xl">Training and Certificates</h1>
-      <div className="flex flex-row gap-4 flex flex-wrap">
+      <div className="flex flex-row gap-4  flex-wrap">
         <div className="bg-white w-[323px] h-[200px]"></div>
         <div className="bg-white w-[323px] h-[200px]"></div>
         <div className="bg-white w-[323px] h-[200px]"></div>
