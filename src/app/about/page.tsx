59fbb3db2fab3b11f02b5cc9e194874718f11fb4
Sabadomarc13 <sabado@pinyastudio.com>
2024-05-17 11:53:53 +0800

    modified:   src/app/about/page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   
   return (
     <div className="px-10 py-9 w-full">
-      <h1 className="font-semibold text-3xl">About</h1>
+      <h1 className="font-semibold text-3xl">About</h1>\
+      <p>ajkshndjkashjdhjashjdhjkashdjhjjnasdncjndncjnwjedncjnwejnfjndja</p>
     </div>
   );
 }
